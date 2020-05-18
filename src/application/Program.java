ac845ea9af1664d2cd2d415702026741d8cb9bdb
Luiz <lhserafim@gmail.com>
2020-05-18 16:35:53 -0300

    Projeto atualizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,10 +13,17 @@ public class Program {
         myShapes.add(new Rectangle(3.0, 2.0));
         myShapes.add(new Circle(2.0));
 
+        List<Circle> myCircles = new ArrayList<>();
+        myCircles.add(new Circle(2.0));
+        myCircles.add(new Circle(3.0));
+
         System.out.println("Total area: " + totalArea(myShapes));
+        System.out.println("Total area: " + totalArea(myCircles));
     }
 
-    public static double totalArea(List<Shape> list) {
+    //public static double totalArea(List<Shape> list) {
+    //para poder usar a função totalArea para Circle e Shape, necessário
+    public static double totalArea(List<? extends Shape> list) {
         double sum = 0.0;
         for (Shape s : list) {
             sum += s.area();
